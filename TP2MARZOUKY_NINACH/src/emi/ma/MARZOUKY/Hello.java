8f96e81fda8aaec689ad178cd2f751dbe4f7ef7a
LOUBNAMARZOUKY <loubnamarzouky0@gmail.com>
2018-11-28 10:55:01 +0100

    r2

--- TP2MARZOUKY_NINACH/src/emi/ma/MARZOUKY/Hello.java
+++ TP2MARZOUKY_NINACH/src/emi/ma/MARZOUKY/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, je suis MARZOUKY Loubna!");
+		System.out.println("Hello, je suis MARZOUKY Loubna!");
 
 	}
 
