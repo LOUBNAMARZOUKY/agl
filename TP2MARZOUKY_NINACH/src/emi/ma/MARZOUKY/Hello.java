b4392c77c9345b104fb9c4e19865b25b9525fe11
LOUBNAMARZOUKY <loubnamarzouky0@gmail.com>
2018-11-28 10:50:05 +0100

    Création du projet

---
+++ TP2MARZOUKY_NINACH/src/emi/ma/MARZOUKY/Hello.java
@@ -0,0 +1,11 @@
+package emi.ma.MARZOUKY;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, je suis MARZOUKY Loubna!");
+
+	}
+
+}
